9c7cb9fe8842f515fc4f2841223dc2f85bb8ea90
Angela Wang <angeladanwang@gmail.com>
2015-03-30 13:57:45 -0700

    Pass all tests

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,17 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = [];
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards instead";
+};
+RetiredForagerBee.prototype.gamble(treasure) {
+  this.treasureChest.push(treasure);
+}
